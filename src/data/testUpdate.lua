e82eece0883457806ce18de271fce7554dd38289
U-2013-20151122YR\Administrator <a5323781>
2016-06-16 19:51:16 +0800

    测试热更

--- src/data/testUpdate.lua
+++
@@ -1,6 +0,0 @@
-testUpdate = {}
-testUpdate.tt = function()
-	print("这是热更文件")
-end
-
-return testUpdate
---
+++ src/login/LoginView.lua
@@ -0,0 +1,13 @@
+LoginView = class("LoginView", LayerBase)
+
+
+function LoginView:create()
+	local ret = LoginView.new()
+	return ret
+end
+
+function LoginView:init()
+
+	print("this is hot update file")
+	
+end
